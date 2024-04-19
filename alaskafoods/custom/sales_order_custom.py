0b515b427e429d8bee14b32bda22fa7717d42e22
safdar211ali <safdar211@hotmail.com>
2024-04-19 13:53:38 +0500

    updated

--- alaskafoods/custom/sales_order_custom.py
+++ alaskafoods/custom/sales_order_custom.py
@@ -41,7 +41,7 @@ def after_insert(doc, method):
             it.qty = items.qty
             it.rate = items.rate
             it.amount = items.amount
-        si.reload()
+            it.so_detail = items.name
         si.submit()
 
     except Exception as e:
