a8c8b38f249272534e1a288e30e88d4646b2191e
safdar211ali <safdar211@hotmail.com>
2024-04-22 14:55:34 +0500

    updated

---
+++ alaskafoods/alaskafoods/doctype/load_form/load_form.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, techrix and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Load Form", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ alaskafoods/alaskafoods/doctype/load_form/test_load_form.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, techrix and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLoadForm(FrappeTestCase):
+	pass
