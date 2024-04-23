d06a4fa26d4f92b1058de8bf8977a3e2c5db6143
safdar211ali <safdar211@hotmail.com>
2024-04-23 16:43:20 +0500

    updated

--- alaskafoods/alaskafoods/doctype/load_form/load_form.js
+++ alaskafoods/alaskafoods/doctype/load_form/load_form.js
@@ -1,8 +1,46 @@
 // Copyright (c) 2024, techrix and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Load Form", {
-// 	refresh(frm) {
+frappe.ui.form.on("Load Form", {
+    refresh(frm) {
 
-// 	},
-// });
+    },
+    sales_person: function (frm) {
+        var sales_person = frm.doc.sales_person;
+        var delivery_date = frm.doc.delivery_date;
+        fill_load_form(frm, sales_person, delivery_date);
+
+    },
+});
+
+function fill_load_form(frm, sales_person, delivery_date) {
+    if (sales_person && delivery_date) {
+        // Clear existing data before adding new entries
+        frm.clear_table("load_form_items");
+
+        frappe.call({
+            method: "alaskafoods.alaskafoods.utils.fill_load_form.fill_load_form",
+            args: {
+                sales_person: sales_person,
+                delivery_date: delivery_date
+            },
+            callback: function (response) {
+                if (response.message.load_form_item_data) {
+                    response.message.load_form_item_data.forEach(function (i) {
+                        let entry = frm.add_child("load_form_items");
+                        entry.item_code = i.item_code,
+                        entry.item_name = i.item_name,
+                        entry.issued_units = i.issued_units,
+                        entry.free_units = i.free_units,
+                        entry.total_units = i.total_units,
+                        entry.carton = i.carton,
+                        entry.units = i.units,
+                        entry.pack_size = i.conversion_factor
+
+                    });
+                }
+                frm.refresh_field('load_form_items');
+            }
+        });
+    }
+}
---
+++ alaskafoods/alaskafoods/utils/fill_load_form.py
@@ -0,0 +1,57 @@
+import frappe
+
+
+@frappe.whitelist()
+def fill_load_form(**args):
+    sales_person = args.get('sales_person')
+    delivery_date = args.get('delivery_date')
+
+    sales_invoice_items = """
+    SELECT
+        sii.item_code,
+        sii.item_name,
+        sii.uom,
+        SUM(CASE WHEN sii.qty > 0 AND sii.uom = 'Pcs' THEN sii.qty ELSE 0 END) AS issued_units,
+        SUM(CASE WHEN sii.qty > 0 AND sii.uom = 'Carton' THEN sii.qty ELSE 0 END) AS carton,
+        SUM(CASE WHEN sii.qty < 0 THEN ABS(sii.qty) ELSE 0 END) AS free_units,
+        (SUM(CASE WHEN sii.qty > 0 AND sii.uom = 'Pcs' THEN sii.qty ELSE 0 END) + SUM(CASE WHEN sii.qty < 0 THEN ABS(sii.qty) ELSE 0 END)) AS total_units,
+        sii.conversion_factor,
+        CASE WHEN sii.conversion_factor = 0 THEN 1 ELSE sii.conversion_factor END AS pack_size,
+        0 AS units
+    FROM
+        `tabSales Invoice` AS si
+    INNER JOIN 
+        `tabSales Invoice Item` AS sii ON si.name = sii.parent
+    WHERE
+        si.docstatus = 1 AND
+        si.custom_sales_person = %s AND 
+        si.posting_date = %s
+    GROUP BY
+        sii.item_code,sii.conversion_factor,sii.item_name
+    """
+    load_form_item_data = frappe.db.sql(sales_invoice_items, (sales_person, delivery_date,), as_dict=True)
+    for i in load_form_item_data:
+        if i['uom'] == 'Pcs':
+            i['carton'] = pcs_to_carton(i['total_units'], i['item_code']).get('carton')
+            i['units'] = pcs_to_carton(i['total_units'], i['item_code']).get('units')
+
+    return {
+        "load_form_item_data": load_form_item_data,
+    }
+
+
+def pcs_to_carton(qty=0, item_code=None):
+    carton = 0
+    units = 0
+    conversion_factor = frappe.db.get_value("UOM Conversion Detail",
+                                            {"parent": item_code, "uom": "Carton"},
+                                            "conversion_factor")
+    if qty > 0 and item_code:
+        carton = qty // conversion_factor
+        result = qty / conversion_factor
+        decimal_portion = result % 1
+        units = int(decimal_portion * conversion_factor)
+    return {
+        "carton": carton,
+        "units": units
+    }
